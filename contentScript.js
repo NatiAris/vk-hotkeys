7433e1c71f55e0b372567a214e0b9424190c027b
NatiAris <NatiAris@users.noreply.github.com>
2018-07-02 00:45:20 +0300

    Fixing the like button binding (was broken due to changes in the website)

--- contentScript.js
+++ contentScript.js
@@ -3,8 +3,8 @@ document.onkeydown = function(event) {
         event = window.event;
     var k = event.keyCode;
     switch(k) {
-        case 38: // Up
-            document.getElementById('pv_like').click()
+        case 38: // Up Arrow
+            document.getElementsByClassName('like_btn like _like  ')[0].click()
             break;
         case 40: // Down
             document.getElementById('pv_save_to_me').click()
