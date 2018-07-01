f7bbf20b7c76160324c805958405351bea8c7957
NatiAris <NatiAris@users.noreply.github.com>
2018-07-02 00:43:06 +0300

    Removing redundant code

--- contentScript.js
+++ contentScript.js
@@ -2,9 +2,6 @@ document.onkeydown = function(event) {
     if (!event)
         event = window.event;
     var k = event.keyCode;
-    // I don't have the slightest idea what the next two lines do.
-    if (event.charCode && k == 0)
-        k = event.charCode;
     switch(k) {
         case 38: // Up
             document.getElementById('pv_like').click()
