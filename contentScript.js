0c81dc1446fc25709ff1bf0b952be144909094f3
NatiAris <NatiAris@users.noreply.github.com>
2018-09-24 21:11:49 +0300

    Commenting out laggy like/dislike part

--- contentScript.js
+++ contentScript.js
@@ -3,9 +3,9 @@ document.onkeydown = function(event) {
         event = window.event;
     var k = event.keyCode;
     switch(k) {
-        case 38: // Up Arrow
-            document.getElementsByClassName('like_btn like _like  ')[0].click()
-            break;
+        //case 38: // Up Arrow
+        //    document.getElementsByClassName('like_btn like _like  ')[0].click()
+        //    break;
         case 40: // Down Arrow
             save_button = document.getElementById('pv_save_to_me')
             remove_button = document.getElementById('pv_delete')
